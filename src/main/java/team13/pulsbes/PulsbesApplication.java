3e29384acd2cd82336642e4e431199fd01a676e6
LORENZO LANARI s203515 <s203515@studenti.polito.it>
2020-11-06 17:34:47 +0100

    code: project setup with pom dependencies

---
+++ src/main/java/team13/pulsbes/PulsbesApplication.java
@@ -0,0 +1,13 @@
+package team13.pulsbes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsbesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsbesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team13/pulsbes/PulsbesApplicationTests.java
@@ -0,0 +1,13 @@
+package team13.pulsbes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsbesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
