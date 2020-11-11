deca4148a6f1647c68fdaec39eb34a2ff16ae27f
LORENZO LANARI s203515 <s203515@studenti.polito.it>
2020-11-11 16:46:39 +0100

    code: function for getting number of students for a lecture

--- src/main/java/team13/pulsbes/dtos/LectureDTO.java
+++ src/main/java/team13/pulsbes/dtos/LectureDTO.java
@@ -2,10 +2,15 @@ package team13.pulsbes.dtos;
 
 
 import lombok.Data;
+import team13.pulsbes.entities.Student;
+
 import org.hibernate.exception.DataException;
 
 import javax.persistence.Id;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class LectureDTO {
@@ -26,4 +31,63 @@ public class LectureDTO {
     private Integer TotalSeat;
 
     private String RoomName;
+    private List<Student> students;
+    {
+        students = new ArrayList<>();
+    }
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public Date getStartTime() {
+		return StartTime;
+	}
+	public void setStartTime(Date startTime) {
+		StartTime = startTime;
+	}
+	public Date getEndTime() {
+		return EndTime;
+	}
+	public void setEndTime(Date endTime) {
+		EndTime = endTime;
+	}
+	public String getLectureType() {
+		return LectureType;
+	}
+	public void setLectureType(String lectureType) {
+		LectureType = lectureType;
+	}
+	public String getSurnameString() {
+		return SurnameString;
+	}
+	public void setSurnameString(String surnameString) {
+		SurnameString = surnameString;
+	}
+	public Integer getAvailableSeat() {
+		return AvailableSeat;
+	}
+	public void setAvailableSeat(Integer availableSeat) {
+		AvailableSeat = availableSeat;
+	}
+	public Integer getTotalSeat() {
+		return TotalSeat;
+	}
+	public void setTotalSeat(Integer totalSeat) {
+		TotalSeat = totalSeat;
+	}
+	public String getRoomName() {
+		return RoomName;
+	}
+	public void setRoomName(String roomName) {
+		RoomName = roomName;
+	}
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+    
 }
--- src/main/java/team13/pulsbes/services/TeacherService.java
+++ src/main/java/team13/pulsbes/services/TeacherService.java
@@ -1,5 +1,15 @@
 package team13.pulsbes.services;
 
-public interface TeacherService {
+import java.util.List;
 
+import team13.pulsbes.dtos.LectureDTO;
+import team13.pulsbes.dtos.TeacherDTO;
+import team13.pulsbes.entities.Lecture;
+import team13.pulsbes.entities.Student;
+import team13.pulsbes.exception.InvalidTeacherException;
+
+public interface TeacherService {
+	
+	Integer getStudentsAttending(LectureDTO l) throws InvalidTeacherException;
+	void mailForNumber(TeacherDTO tDto);
 }
