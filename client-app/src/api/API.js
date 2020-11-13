975c5599916196704982f8b58a38849f7b881cb4
Luca <luca97vale@gmail.com>
2020-11-13 14:50:06 +0100

    login API works

--- client-app/src/api/API.js
+++ client-app/src/api/API.js
@@ -2,3 +2,26 @@ const baseURL = "/api";
 
 const API = {} ;
 export default API;
+
+async function userLogin(email, psw, type) {
+    return new Promise((resolve, reject) => {
+        fetch(baseURL + '/login/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email: email, psw: psw, teacher: type == "teacher" ? 1 : 0}),
+        }).then((response) => {
+            if (response.ok) {
+                response.json().then((user) => {
+                    resolve(user);
+                });
+            } else {
+                // analyze the cause of error
+                response.json()
+                    .then((obj) => { reject(obj); }) // error msg in the response body
+                    .catch((err) => { reject({ errors: [{ param: "Application", msg: "Cannot parse server response" }] }) }); // something else
+            }
+        }).catch((err) => { reject({ errors: [{ param: "Server", msg: "Cannot communicate" }] }) }); // connection errors
+    });
+}
