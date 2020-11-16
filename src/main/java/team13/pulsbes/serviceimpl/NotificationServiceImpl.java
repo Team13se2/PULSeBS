26685c90a9ba290a197705169d4893ba80a4878f
Matteo Pellegrino <s267652@studenti.polito.it>
2020-11-16 10:10:09 +0100

    email update

---
+++ src/main/java/team13/pulsbes/serviceimpl/NotificationServiceImpl.java
@@ -0,0 +1,26 @@
+package team13.pulsbes.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import team13.pulsbes.services.NotificationService;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    @Autowired
+    JavaMailSender emailSender;
+
+
+    @Override
+    public void sendMessage(String address, String subject, String body) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("team13se2@gmail.com");
+        message.setTo(address);
+        message.setSubject(subject);
+        message.setText(body);
+
+    }
+}
