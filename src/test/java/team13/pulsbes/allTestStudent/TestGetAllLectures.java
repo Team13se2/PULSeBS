3201bf3a538cdc87ca0ed0a53fb798813c4e7836
LORENZO LANARI s203515 <s203515@studenti.polito.it>
2020-11-19 15:09:59 +0100

    test: teacher and student service impl

--- src/test/java/team13/pulsbes/allTestStudent/TestGetAllLectures.java
+++ src/test/java/team13/pulsbes/allTestStudent/TestGetAllLectures.java
@@ -8,25 +8,30 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 
+import team13.pulsbes.dtos.LectureDTO;
 import team13.pulsbes.entities.Course;
 import team13.pulsbes.entities.Lecture;
 import team13.pulsbes.entities.Student;
+import team13.pulsbes.exception.InvalidCourseException;
 import team13.pulsbes.exception.InvalidStudentException;
 import team13.pulsbes.repositories.StudentRepository;
 import team13.pulsbes.serviceimpl.StudentServiceImpl;
-import team13.pulsbes.services.StudentService;
 
 class TestGetAllLectures {
 
-	StudentService studentService;
+	StudentServiceImpl studentService;
 	StudentRepository studentRepository;
+	ModelMapper modelMapper;
 	
 	@BeforeEach
 	void setUp() {
 		studentRepository = mock(StudentRepository.class);
+		modelMapper = mock(ModelMapper.class);
 		studentService = new StudentServiceImpl();
-		
+		studentService.addStudentRepo(studentRepository);
+		studentService.addModelMapper(modelMapper);
 	}
 	
 	@Test
@@ -34,17 +39,22 @@ class TestGetAllLectures {
 		assertThrows(InvalidStudentException.class,()->studentService.getAllLectures("-1"));
 	}
 	@Test
-	void testGetAllLectures2() {
+	void testGetAllLectures2() throws InvalidStudentException, InvalidCourseException {
 		Student s = new Student("1","name","surname");
-		List<Course> courses = new ArrayList<>();
 		Course c = new Course();
 		Lecture l = new Lecture();
+		l.setId("1");
 		List<Lecture> lectures = new ArrayList<>();
 		lectures.add(l);
 		c.setLectures(lectures);
-		courses.add(c);
-		s.setCourses(courses);
+		s.addCourse(c);
 		when(studentRepository.getOne(any())).thenReturn(s);
-		
+		List<LectureDTO> ret = new ArrayList<>();
+		LectureDTO lDto = new LectureDTO();
+		lDto.setId("1");
+		ret.add(lDto);
+		studentService.getAllLectures("1");
+		when(modelMapper.map(any(), any())).thenReturn(lDto);
+		assertEquals(ret.get(0).getId(), studentService.getAllLectures("1").get(0).getId());
 	}
 }
