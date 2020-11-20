58b61b6facf0fd9263728491126cd50c7f93cacc
Luca <luca97vale@gmail.com>
2020-11-20 10:34:08 +0100

    fix some bugs in client-app

--- client-app/src/components/TeacherPage.js
+++ client-app/src/components/TeacherPage.js
@@ -29,6 +29,8 @@ class TeacherPage extends React.Component {
   getAllLectures = () =>{
     API.getAllLectures().then((lecture) =>{
         this.setState({lectures: lecture});
+    }).catch((err) =>{
+      console.log(err);
     })
 }
 
