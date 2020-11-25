8c0faf8cf478fb0ff59687a7fe11de353d28f045
FraCampa <s253669@studenti.polito.it>
2020-11-25 10:26:27 +0100

    cancelLecture for teacher

--- src/main/java/team13/pulsbes/controllers/TeacherController.java
+++ src/main/java/team13/pulsbes/controllers/TeacherController.java
@@ -10,6 +10,7 @@ import team13.pulsbes.dtos.StudentDTO;
 import team13.pulsbes.dtos.TeacherDTO;
 import team13.pulsbes.entities.Lecture;
 import team13.pulsbes.exception.InvalidLectureException;
+import team13.pulsbes.exception.InvalidCourseException;
 import team13.pulsbes.exception.InvalidTeacherException;
 import team13.pulsbes.services.TeacherService;
 import team13.pulsbes.utils.Constants;
@@ -22,9 +23,13 @@ public class TeacherController {
 	TeacherService teacherService;
 	
 	@RequestMapping(value = Constants.GET_NUMBER_STUDENTS_ATTENDING,method = RequestMethod.GET)
-	public Integer getNumberStudentsAttending(@RequestParam("lecture_id") String id) throws InvalidLectureException {
+	public Integer getNumberStudentsAttending(@RequestParam("lecture_id") String id, @CookieValue(value = "type") String type) throws InvalidLectureException {
 		try {
-			return teacherService.getNumberStudentsAttending(id);
+			if (type == "teacher") {
+				return teacherService.getNumberStudentsAttending(id);
+			}
+			else return 0;
+			
 		} catch (InvalidLectureException e) {
 			
 			System.out.println(e.getMessage());
@@ -32,24 +37,44 @@ public class TeacherController {
 		}
 	}
 	@RequestMapping(value = Constants.GET_ALL_LECTURES, method = RequestMethod.GET)
-	public List<LectureDTO> getAllLectures(@CookieValue(value = "username") String username,@CookieValue(value = "id") String id) throws InvalidTeacherException{
+	public List<LectureDTO> getAllLectures(@CookieValue(value = "username") String username,@CookieValue(value = "id") String id, @CookieValue(value = "type") String type) throws InvalidTeacherException{
 		try {
+			if (type == "teacher") {
 			List<LectureDTO> l = teacherService.getAllLectures(id);
 			for(int i=0;i<l.size();i++){
 				Integer nr = teacherService.getNumberStudentsAttending(l.get(i).getId());
 				l.get(i).setNrStudents(nr);
 			}
 			return l;
+		}
+		else return null;
+		
 		} catch (InvalidTeacherException | InvalidLectureException e) {
 			System.out.println(e.getMessage());
 			return null;
 		}
 	}
 	@RequestMapping(value = Constants.GET_STUDENT_LIST,method = RequestMethod.GET)
-	public List<StudentDTO> getStudentList(@RequestParam("lecture_id") String id) throws InvalidLectureException {
+	public List<StudentDTO> getStudentList(@RequestParam("lecture_id") String id, @CookieValue(value = "type") String type) throws InvalidLectureException {
 		try {
+			if (type == "teacher") {
 			return teacherService.getStudentList(id);
+			}
+			else return null;
 		} catch (InvalidLectureException e) {
+			
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	@RequestMapping(value = Constants.CANCEL_LECTURE,method = RequestMethod.GET)
+	public String cancelLecture(@RequestParam("lecture_id") String lectureId, @RequestParam("course_id") String courseId, @CookieValue(value = "type") String type) throws InvalidLectureException, InvalidCourseException {
+		try {
+			if (type == "teacher") {
+			return teacherService.cancelLecture(lectureId, courseId);
+			}
+			else return null;
+		} catch (InvalidLectureException | InvalidCourseException e) {
 			
 			System.out.println(e.getMessage());
 			return null;
--- src/main/java/team13/pulsbes/services/TeacherService.java
+++ src/main/java/team13/pulsbes/services/TeacherService.java
@@ -9,10 +9,12 @@ import team13.pulsbes.entities.Lecture;
 import team13.pulsbes.entities.Student;
 import team13.pulsbes.exception.InvalidLectureException;
 import team13.pulsbes.exception.InvalidTeacherException;
+import team13.pulsbes.exception.InvalidCourseException;
 
 public interface TeacherService {
 	
 	Integer getNumberStudentsAttending(String id) throws InvalidLectureException;
 	List<LectureDTO> getAllLectures(String id) throws InvalidTeacherException;
 	List<StudentDTO> getStudentList(String id) throws InvalidLectureException;
+	String cancelLecture(String lectureId, String courseId) throws InvalidLectureException, InvalidCourseException;
 }
